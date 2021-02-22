139075d1465254831dabcb262c0769ee53d07cf8
Louise Stafford <G00398255@gmit.ie>
2021-02-22 01:24:22 +0000

    week 02

---
+++ week02/addOne.py
@@ -0,0 +1,3 @@
+#Author: Louise Stafford
+name = input ("Enter your name:")
+print ( 'Hello ' + name '\nNice to meet you')
---
+++ week02/hello2.py
@@ -0,0 +1,4 @@
+#Lab 2.2 continued - creating a new file that reads a persons name & outputs same
+#Author: Louise Stafford
+name = input ("Enter your name:")
+print ( 'Hello ' + name)
